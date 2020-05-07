d0e8a7df4597021b5b23db77418b9c1c83b28d31
Tomas Baronas <tomas.r.baronas@gmail.com>
2020-05-07 14:37:57 +0200

    add tachyons and refactor app.js

--- App.js
+++ App.js
@@ -7,14 +7,8 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, ScrollView, View, Text, StatusBar} from 'react-native';
+import {styles as s} from 'react-native-style-tachyons';
 
 import {
   Header,
@@ -31,36 +25,38 @@ const App: () => React$Node = () => {
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+          style={{backgroundColor: Colors.lighter}}>
           <Header />
           {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+            <View style={[s.absolute, s.right_0]}>
+              <Text style={[s.tr, s.f5, s.fw6, s.p6, s.pr5]}>
+                Engine: Hermes
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
+          )}
+          <View style={{backgroundColor: Colors.white}}>
+            <View style={[s.mt3, s.ph4]}>
+              <Text style={[s.f4, s.b]}>Step One</Text>
+              <Text style={[s.mt1, s.f5, s.black, s.fw4]}>
+                Edit <Text style={[s.fw7]}>App.js</Text> to change this screen
+                and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={[s.mt3, s.ph4]}>
+              <Text style={[s.f4, s.b]}>See Your Changes</Text>
+              <Text style={[s.mt1, s.f5, s.black, s.fw4]}>
                 <ReloadInstructions />
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
+            <View style={[s.mt3, s.ph4]}>
+              <Text style={[s.f4, s.b]}>Debug</Text>
+              <Text style={[s.mt1, s.f5, s.black, s.fw4]}>
                 <DebugInstructions />
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
+            <View style={[s.mt3, s.ph4]}>
+              <Text style={[s.f4, s.b]}>Learn More</Text>
+              <Text style={[s.mt1, s.f5, s.black, s.fw4]}>
                 Read the docs to discover what to do next:
               </Text>
             </View>
@@ -71,44 +67,5 @@ const App: () => React$Node = () => {
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Dimensions, StyleSheet} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import NativeTachyons from 'react-native-style-tachyons';
+
+NativeTachyons.build(
+  {
+    rem: Dimensions.get('window').width > 340 ? 18 : 16,
+    fontRem: 20,
+  },
+  StyleSheet,
+);
 
 AppRegistry.registerComponent(appName, () => App);
